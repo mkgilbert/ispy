614528ae2f54523fdd4cae794ba761e11b06685b
TannerStevens <tanner.stevens42@gmail.com>
2016-10-30 19:12:45 -0700

    Initial Layout

--- index.android.js
+++ index.android.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
   View
@@ -15,39 +16,51 @@ import {
 export default class ispy extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+    	<View style={styles.mainContainer}>
+    		<View style={styles.sidebar}>
+    			<MyComponent/>
+    		</View>
+    		<View style={styles.content}>
+    			<Text>Testing.</Text>
+    			<Text>Testing.</Text>
+    			<Text>Testing.</Text>
+    			<Text>Testing.</Text>
+    		</View>
+    	</View>
     );
   }
 }
 
+class MyComponent extends Component {
+	constructor() {
+		super();
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+		};
+	}
+
+	render() {
+		return (
+			<ListView
+				dataSource={this.state.dataSource}
+				renderRow={(rowData) => <Text>{rowData}</Text>}
+			/>
+		);
+	}
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	sidebar: {
+  		flexDirection: 'column'
+  	},
+  	mainContainer: {
+    	flex: 1,
+    	flexDirection: 'row'
+  	},
+  	content: {
+  		flex: 1
+  	},
 });
 
 AppRegistry.registerComponent('ispy', () => ispy);
